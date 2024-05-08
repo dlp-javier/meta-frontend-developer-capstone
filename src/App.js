abd05303b9a689adcf8d5951135dc3cd9ba4e04f
dlp-javier <dlp.javier197@gmail.com>
2024-05-07 22:22:41 -0400

    Import of NAV component into app.js and NAV file HTML layout for navigation bar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
+import Nav from './components/Nav'
 
 function App() {
   return (
-    <h1>Meta front end developer capstone starter files</h1>
+    <Nav />
   );
 }
 
